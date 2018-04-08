e58fbe7c80475018ec1bc0195c80b8037124349b
Th13um <dthieum@gmail.com>
2018-04-08 10:50:40 +0200

    correction after test the files on validator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length,
+  let currentIndex = array.length,
     temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
@@ -13,17 +13,17 @@ function shuffle(array) {
   return array;
 }
 
-const cardList = document.getElementsByClassName("card");
+const cardList = document.getElementsByClassName('card');
 const newCardList = Array.prototype.slice.call(cardList);
 const shuffledCard = shuffle(newCardList);
 
-let newList = "";
+let newList = '';
 for (i = 0; i < shuffledCard.length; i++) {
   newList += shuffledCard[i].outerHTML;
 }
 
 function shuffleDeck() {
-  document.getElementById("deck").innerHTML = newList;
+  document.getElementById('deck').innerHTML = newList;
 }
 
 shuffleDeck();
@@ -39,60 +39,57 @@ let moveCount = 0;
 function updateMoveCount() {
   moveCount += 1;
   if (moveCount <= 1) {
-    document.getElementById("moves").innerHTML = moveCount + " move";
+    document.getElementById('moves').innerHTML = moveCount + ' move';
   } else {
-    document.getElementById("moves").innerHTML = moveCount + " moves";
+    document.getElementById('moves').innerHTML = moveCount + ' moves';
   }
   adjustStars();
   victory();
 }
 
 function matchCards() {
-  matchedCount += 1;
   setTimeout(function() {
-    matchingCards = document.querySelectorAll(".show");
+    matchingCards = document.querySelectorAll('.show');
     for (let i = 0; i < matchingCards.length; i++) {
-      matchingCards[i].className = "card match";
+      matchingCards[i].className = 'card match';
     }
   }, 500);
+  matchedCount += 1;
 }
 
 function hideCards() {
   visibleCard = [];
-  displayedCards = document.querySelectorAll(".show");
+  displayedCards = document.querySelectorAll('.show');
+  for (let i = 0; i < displayedCards.length; i++) {
+    displayedCards[i].className += ' wrong';
+  }
   setTimeout(function() {
     for (let i = 0; i < displayedCards.length; i++) {
-      displayedCards[i].className += " wrong";
-    }
-  }, 500);
-  setTimeout(function() {
-    for (let i = 0; i < displayedCards.length; i++) {
-      displayedCards[i].className = "card hidden";
+      displayedCards[i].className = 'card hidden';
     }
   }, 1500);
   updateMoveCount();
 }
 
 function revealCard(e) {
-  e.target.classList.add("show", "open");
-  e.target.classList.remove("hidden");
+  e.target.classList.add('show', 'open');
+  e.target.classList.remove('hidden');
 }
 
 function faceUpCard(e) {
-  let classMatch = e.target.querySelector("i").className;
+  let classMatch = e.target.querySelector('i').className;
   visibleCard.push(classMatch);
 }
 
-document.body.addEventListener("click", function(e) {
-  if (e.target.classList.contains("hidden")) {
-    if (document.querySelectorAll(".hidden").length == 16 && moveCount === 0) {
-
-      const cards = document.getElementsByClassName("card");
+document.body.addEventListener('click', function(e) {
+  if (e.target.classList.contains('hidden')) {
+    if ((document.querySelectorAll('.hidden').length == 16) && (moveCount === 0)) {
+      const cards = document.getElementsByClassName('card');
       cards.onClick = startCount();
     }
 
-      let classMatch = e.target.querySelector("i").className;
-      let matchedCards = [];
+    let classMatch = e.target.querySelector('i').className;
+    let matchedCards = [];
 
     if (visibleCard.length < 1) {
       revealCard(e);
@@ -114,27 +111,27 @@ document.body.addEventListener("click", function(e) {
 
 // Restart game function
 
-const restartButton = document.getElementsByClassName("restart")[0];
+const restartButton = document.getElementsByClassName('restart')[0];
 
 function restartGame() {
   document.location.reload();
 }
 
-restartButton.addEventListener("click", restartGame);
+restartButton.addEventListener('click', restartGame);
 
 // Stars rating system
 
-let starRating = document.getElementsByClassName("stars")[0];
-let starCount = "3 stars";
+let starRating = document.getElementsByClassName('stars')[0];
+let starCount = '3 stars';
 
 function adjustStars() {
   if (moveCount > 10) {
-    starRating.innerHTML = "<li><i class='fas fa-star'></i></li><li><i class='fas fa-star'></i></li><li><i class='far fa-star'></i></li>";
-    starCount = "2 stars";
+    starRating.innerHTML = '<li><i class=\'fas fa-star\'></i></li><li><i class=\'fas fa-star\'></i></li><li><i class=\'far fa-star\'></i></li>';
+    starCount = '2 stars';
   }
   if (moveCount > 20) {
-    starRating.innerHTML = "<li><i class='fas fa-star'></i></li><li><i class='far fa-star'></i></li><li><i class='far fa-star'></i></li>";
-    starCount = "1 star";
+    starRating.innerHTML = '<li><i class=\'fas fa-star\'></i></li><li><i class=\'far fa-star\'></i></li><li><i class=\'far fa-star\'></i></li>';
+    starCount = '1 star';
   }
 }
 
@@ -168,17 +165,17 @@ function stopCount() {
 
 function victoryAlert() {
   sweetAlert({
-      title: "Good job!",
-      text: "You finish in " + (seconds - 1) + "s with " + starCount,
-      icon: "success",
-      buttons: ["Cancel", "Restart ?"]
+      title: 'Good job!',
+      text: 'You finish in ' + (seconds - 1) + 's with ' + starCount,
+      icon: 'success',
+      buttons: ['Cancel', 'Restart ?'],
 
     })
     .then((restart) => {
       if (restart) {
         restartGame();
       } else {
-        swal("Have a nice day!");
+        swal('Have a nice day!');
       }
     });
 }
